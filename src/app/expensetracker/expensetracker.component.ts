df5b6755a79bb4aa411eb53e13b96978fbd709e7
thembalakhe <m.themba1738@gmail.com>
2025-01-09 14:38:59 +0200

    updated ui, added more input fields for more data intake,

--- src/app/expensetracker/expensetracker.component.ts
+++ src/app/expensetracker/expensetracker.component.ts
@@ -1,47 +1,79 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgClass, NgFor } from '@angular/common';
+import { CommonModule} from '@angular/common';
+
+export interface Item {
+  title: string | null;
+  amount: number | null;
+  date: string | null;
+  itemType: string;
+  category: string;
+  payMethod: string;
+  description: string | null;
+}
 
 @Component({
   selector: 'app-expensetracker',
   standalone: true,
-  imports: [FormsModule, NgFor,NgClass],
+  imports: [FormsModule, CommonModule],
   templateUrl: './expensetracker.component.html',
-  styleUrl: './expensetracker.component.css'
+  styleUrl: './expensetracker.component.css',
 })
 export class ExpensetrackerComponent {
+  totalExpense: number = 0;
+  totalIncome: number = 0;
+  net: number = 0;
 
-  expenses : any [] = [];
-  expenseItem : string =  '';
-  amount: number = 0;
-  expenseType :string = ''
-  totalExpense : number = 0 ;
-  totalIncome :number = 0
-  net : number  = 0 
+  title: string | null = null;
+  amount: number | null = null;
+  date: string | null = null;
+  itemType: string = '';
+  category: string = '';
+  payMethod: string = '';
+  description: string | null = null;
 
-  add (){
+  items: Item[] = [];
+  categories = [
+    'Food',
+    'Rent',
+    'Transport',
+    'Insurance',
+    'Personal Care',
+    'Online',
+    'Household',
+    'Entertainment',
+    'Other',
+  ];
+  expenseTypes = ['Income', 'Expense'];
+  paymentMethods = [
+    'Cash Payment',
+    'Online / EFT',
+    'Credit Card',
+    'Debit Card',
+  ];
 
-    if (this.expenseType && this.expenseItem && this.amount){
-      
-      if (this.expenseType == "income"){
-        this.totalIncome += this.amount
-        
+  add() {
+    if (this.itemType && this.title && this.amount) {
+      if (this.itemType == 'Income') {
+        this.totalIncome += this.amount;
       } else {
-        this.totalExpense += this.amount
+        this.totalExpense += this.amount;
       }
-      
-      let expense = {
-        expenseItem :this.expenseItem,
-        amount:this.amount ,
-        expenseType : this.expenseType
-      }
-      
-      this.expenses.push(expense);
-      console.log(this.expenses);
-      this.expenseItem  =  '';
-      this.amount = 0;
+
+      let item: Item = {
+        title: this.title,
+        amount: this.amount,
+        date: this.date,
+        itemType: this.itemType,
+        category: this.category,
+        payMethod: this.payMethod,
+        description: this.description,
+      };
+
+      this.items.push(item);
+      console.log(this.items);
+      this.title = '';
+      this.amount = null;
     }
-
   }
-
 }
